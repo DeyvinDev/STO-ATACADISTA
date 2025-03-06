d9038b46352e8833ba2ab527ac27060618b372fb
DeyvinDev <deyvisonbrito20@gmail.com>
2025-03-06 20:53:19 -0300

    Atualizando Js

---
+++ RevisãoJS/calculadoraIMC.js
@@ -0,0 +1,7 @@
+//Exportar função
+export function imc(peso,altura){
+    return peso / (altura * altura)
+}
+peso = 150
+altura = 120
+console.log(resultado)
--- RevisãoJS/revisao.js
+++ RevisãoJS/revisao.js
@@ -1,4 +1,4 @@
-// /*COMENTARIO 
+/*// /*COMENTARIO 
 // COMENTARIO TB
 //VARIÁVEIS
 //var --> Não usa no dia a dia
@@ -40,7 +40,59 @@ gays.push("SIDINEI") // acrescenta na lista
 
 console.log(gays.length)
 
-gays.forEach(gay => console.log(gay))
+gays.forEach(gay => console.log(gay)) */
 
 
+//objetos
+let pessoa = {
+    nome: "cleiton",
+    idade: 20,
+    altura: 130
+} 
+console.log(pessoa.nome)
+pessoa.idade = 40
+console.log(pessoa.idade)
+//Desatribuição de Objetos
+const {peso,altura} = pessoa
+console.log(peso)
+console.log(altura)
+//propagação de objetos
+const  endereço = {
+    cidade: "Rio Branco",
+    UF: "Acre"
+} 
 
+const pessoacompleto = {
+    ...pessoa,
+    ...endereço
+}
+
+console.log(pessoacompleto)
+
+//funções
+//Declaração
+function somar(numA,numB){
+    const resultado = numA + numB
+    console.log(resultado)
+}
+//USO
+somar(2,2)
+somar(7,4)
+somar(9,1)
+
+//função anonima
+
+const subtrair = function (numA, numB){
+    const resultado = numA - numB
+    console.log(resultado)
+}
+subtrair(7,9)
+
+//arrow function
+const multiplicar = (numA, numB) => numA * numB
+multiplicar(7,7)
+console.log(multiplicar)
+
+/*import {imc} from "C:\GAROTO DE PROGRAMA\RevisãoJS\calculadoraIMC.js"
+const res = imc(150,120)
+console.log(res)*/
